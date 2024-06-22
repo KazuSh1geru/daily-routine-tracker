fdc849373be76936e262fffd2466ac152176413e
Takakusagi Kazushi <kazushi-takakusagi@TakakusaginoMacBook-Pro.local>
2024-06-22 17:15:09 +0900

    ページ遷移を実装

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,10 @@
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <a href="/">Back to home</a>
+            </h2>
+        </>
+    )
 }
